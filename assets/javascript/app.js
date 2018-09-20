a74cad4c1b69225c0297fab78ec5916f03e845fc
Tony Lockhart <tony.lockhart@ymail.com>
2018-09-20 17:29:58 -0400

    modified display so divs display inline

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,71 @@
-// Initial array of movies
-var gifs = ["Bruce Lee", "Chuck Norris", "Jet Lee", "Wesley Snipes"];
+// Initial array of gifs
+var gifs = ["Bruce Lee", "Chuck Norris", "Li Lianjie", "Wesley Snipes"];
 
-// displayMovieInfo function re-renders the HTML to display the appropriate content
+// displayGiphyInfo function re-renders the HTML to display the appropriate content
 function displayGiphyInfo() {
 
   var giphy = $(this).attr("data-name");
-  var queryURL = "https://www.omdbapi.com/?t=" + giphy + "&y=&plot=short&apikey=trilogy";
+  var genre = "+Karate";
+  var action1 ="kick";
+  var action2 ="punch";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + giphy + genre+/*"AND" + genre + "AND" + action1+"AND"+action2+*/ "&api_key=WcTKDkD6i55rjVcA6LUFToruFyIIGJ9M";
 
+  queryURL = "https://api.giphy.com/v1/gifs/search?api_key=WcTKDkD6i55rjVcA6LUFToruFyIIGJ9M&q=Li+Lianjie+karate+kungfu&limit=10"
   // Creates AJAX call for the specific movie button being clicked
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    /*Options to look for AJAX Response*/
+    /*Step1: Query Giphy and return result in AJAX Response*/
    /*Option1: Log response*/
+   console.log(giphy);
     console.log(response);
   /*Option2: Log queryURL*/
     console.log(queryURL);
   
-    $('#giphy-view').text(JSON.stringify(response));
+  //Dump response information to screen
+ // $('#gifs-appear-here').text(JSON.stringify(response));
+
+   /*************************************
+    * Step2:DISPLAY ALL IMAGES AND RATINGS
+   **************************************/
+  //Empty Image Div before adding new Images
+  $('#gifs-appear-here').empty();
+
+   var results = response.data;
+   for(var i = 0; i < results.length; i++){
+
+    //Make a div to store the fighter image
+    var $fighterDiv = $('<div>');
+    $fighterDiv.addClass('data-inline');
+
+    //Make a paragraph to store the rating of the image
+    var $p = $('<p>');
+    /*$p.addClass('d-inline');*/
+
+    //Set the inner text of the paragraph to the rating of the image in results[i]
+    var rating = results[i].rating;
+    $p.text(rating);
+
+    //Make an image tag with jQuery and store it in a variable named fighterImage.
+    var $fighterImage = $('<img>');
+
+    //Set the image's src to results[i]'s fixed_height.url.
+    $fighterImage.attr('src', results[i].images.fixed_height_small.url);
+    /*$fighterImage.addClass('image-display');*/
+    $fighterImage.addClass('img-fluid');
+
+    //Append the p variable to the fighterDiv var
+    $fighterDiv.append($p);
+
+    //Append the animalImage variable to the animalDiv variable.
+    $fighterDiv.append($fighterImage);
+
+    //Prepend the fighterDiv variable to the element with an id of gifs-appear-here.
+    $('#gifs-appear-here').prepend($fighterDiv);
+   }
+
+
   });
 }
 
@@ -47,12 +94,13 @@ function renderButtons(){
     }//for
 }
 
-//Event hander that adds new gifs to the array and call renderButtons to re render all buttons
+//Event handler that adds new gifs to the array and call renderButtons to re render all buttons
 $('#add-giphy').on('click', function(event){
     event.preventDefault();
 
     //Grab the input from the textbox
     var $gif = $('#giphy-input').val().trim();
+    console.log("New Gif = "+$gif);
 
     //Push the new gif onto the array
     gifs.push($gif);
